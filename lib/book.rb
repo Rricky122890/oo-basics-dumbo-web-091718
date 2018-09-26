eb23de99b86b0b94d7f3b4f06dbd7f07a6e6ec10
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-09-26 18:14:12 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
+class Book
+attr_reader :title, :author, :page_count, :genre
 
+attr_writer :author, :genre
+
+def page_count=(num)
+  @page_count = num
+end
+
+  def initialize(title)
+    @title = title
+
+  end
+
+  def turn_page
+  puts  "Flipping the page...wow, you read fast!"
+
+  end
+
+end
